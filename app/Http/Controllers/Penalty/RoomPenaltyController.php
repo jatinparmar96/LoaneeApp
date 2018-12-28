12f57aead31cbec2286207a4c0874fa00a30c6d2
Jatin Parmar <jatinparmar96@users.noreply.github.com>
2018-12-28 17:49:21 +0530

    Create Penalty Controller
    Empty File for both Percentage and Room Controllers

---
+++ app/Http/Controllers/Penalty/RoomPenaltyController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Penalty;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RoomPenaltyController extends Controller
+{
+    //
+}
